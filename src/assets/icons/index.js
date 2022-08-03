00896bbe0f3bbadd402635779ad1ae65d86c68ce
DepaniJay <jrdepani@gmail.com>
2022-08-03 19:26:12 +0530

    fix issues

--- src/assets/icons/index.js
+++
@@ -1,3 +0,0 @@
-import { ReactComponent as BlockchainProofIcon } from "./blockchain-icon.svg";
-import { ReactComponent as UploadIcon } from "./upload-icon.svg";
-export { BlockchainProofIcon, UploadIcon };
