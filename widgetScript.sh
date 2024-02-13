1e1e19cfc21b0ab77b98b0c5507d4f1cad6cf3aa
Jay Depani <work.depani@gmail.com>
2024-02-13 16:28:46 +0530

    :art: update the widget script

--- widgetScript.sh
+++ widgetScript.sh
@@ -1,10 +1,10 @@
-yarn build:local
+npm run build:local
 mv ./dist/index.js ./dist/healthloq-widget-local.js
-yarn build:dev
+npm run build:dev
 mv ./dist/index.js ./dist/healthloq-widget-dev.js
-yarn build:qa
+npm run build:qa
 mv ./dist/index.js ./dist/healthloq-widget-qa.js
-yarn build:prod
+npm run build:prod
 cp ./dist/index.js ./dist/healthloq-widget-live.js
 mv ./dist/index.js ./dist/healthloq-widget.js
 mv ./dist/index.css ./dist/healthloq-widget.css
